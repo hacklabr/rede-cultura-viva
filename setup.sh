d9e537c5347fa10294f63ac3b8224c8f108356f7
Lucas Pirola <lucaspirola@gmail.com>
2015-09-14 19:28:47 -0300

    fix setup script

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 cd /var/www/rede-cultura-viva
 git pull origin master
 npm install
-NODE_PATH=./node_modules/:./src/js/ ./node_modules/gulp/bin/gulp.js build
+NODE_PATH=./node_modules/:./src/js/ ./node_modules/gulp/bin/gulp.js buildfy
