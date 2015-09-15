7f9561509a5352f84f802a46cc94c03495b406ee
Lucas Pirola <lucaspirola@gmail.com>
2015-09-15 19:42:32 -0300

    Adicionado campos básicos para os dados entidad

--- src/js/components/Form/SecaoDadosEntidade.js
+++ src/js/components/Form/SecaoDadosEntidade.js
@@ -1,9 +1,19 @@
 var m = require('mithril');
+var Input = require('views/Helpers/Input');
+var Select = require('views/Helpers/Select');
 
 var SecaoDadosEntidade = {
 	controller: function () {},
 	view: function(ctrl) {
-		return m('h3', 'Seção Dados Entidade');
+		return m('div', [
+			m('h3', 'Seção Dados Entidade'),
+			m.component(Select, { label:'Quero ser' , name:'quero-ser'}),
+			m.component(Select, { label:'Tipo de organização' , name:'tipo-organizacao'}),
+			m.component(Input, { label:'CNPJ da entidade' , name:'cnpj'}),
+			m.component(Input, { label:'Razão social da Entidade', 'name':'raza-social'}),
+			m.component(Input, { label:'Nome do representante legal', 'name':'representante-legal'}),
+			m.component(Input, { label:'Nome fantasia', 'name':'nome-fantasia'})
+		]);
 	}
 };
 
